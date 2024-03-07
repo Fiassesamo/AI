bb24c55c6c3ec5e0b49f459e1b42d39866f8f8d4
Fiassesamo <volodymryavorskyi@gmail.com>
2024-03-07 11:10:40 +0100

    fix

--- Titanic/main.py
+++ Titanic/main.py
@@ -1,15 +1,15 @@
 import pandas as pd
-import numpy as np
 import tensorflow as tf
 from tensorflow import keras
 from keras.layers import Dense
+import numpy as np
 
 dataframe = pd.read_csv("titanic.csv")
 input_names = ["Age", "Sex", "Pclass"]
 output_names = ["Survived"]
 
 raw_input_data = dataframe[input_names]
-output_input_data = dataframe[output_names]
+raw_output_data = dataframe[output_names]
 
 max_age = 100
 
@@ -25,17 +25,13 @@ def dataframe_to_dict(df):
         result[column] = values
     return result
 
-def make_supervised(df):
-    raw_input_data = dataframe[input_names]
-    raw_output_data = dataframe[output_names]
+def make_supervised():
     return {"inputs": dataframe_to_dict(raw_input_data),
             "outputs": dataframe_to_dict(raw_output_data)}
-
-
 def encode(data):
     vectors = []
-    for data_names, data_values in data.items():
-        encoded = list(map(encoders[data_names], data_values))
+    for data_name, data_values in data.items():
+        encoded = list(map(encoders[data_name], data_values))
         vectors.append(encoded)
     formatted = []
     for vector_raw in list(zip(*vectors)):
@@ -46,22 +42,24 @@ def encode(data):
         formatted.append(vector)
     return formatted
 
-
-supervised = make_supervised(dataframe)
-encoded_inputs = encode(supervised["inputs"])
-encoded_outputs = encode(supervised["outputs"])
-
+supervised = make_supervised()
+encoded_inputs = np.array(encode(supervised["inputs"]))
+encoded_outputs = np.array(encode(supervised["outputs"]))
 
 x_train = encoded_inputs[:600]
-y_train = encoded_outputs[:600]
-
 x_test = encoded_inputs[600:]
+
+y_train = encoded_outputs[:600]
 y_test = encoded_outputs[600:]
 
 model = keras.Sequential([
-    Dense(units=5, activation="relu"),
-    Dense(units=1, activation="softmax")
+    Dense(units=64, activation="relu"),
+    Dense(units=128, activation="relu"),
+    Dense(units=1, activation="sigmoid")
 ])
 
-model.compile(optimizer="adam",
-              )
+model.compile(loss="mse", optimizer="adam", metrics=["accuracy"])
+
+model.fit(x_train, y_train, validation_split=0.2, epochs=100, batch_size=32)
+
+model.evaluate(x_train, y_train)
